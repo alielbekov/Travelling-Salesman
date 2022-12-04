7b4643cf77fd8fb7e6dd17a499890fbda9a8ff9d
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-12-04 13:58:10 -0700

    add three files

---
+++ TravellingSalesman/DGraph.java
@@ -0,0 +1,39 @@
+
+public class DGraph {
+	
+	
+	
+	
+	public void myOwn() {
+		
+		
+	}
+	
+	
+	
+	public void BackTracking() {
+		
+	}
+	
+	
+	
+	public void Heruistic() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	class Graph{
+		
+//		A weighted directed graph is needed to represent the ‘cities’ and their distances. The graph
+//		should support operations that will be needed by the algorithms, mainly a method to find all
+//		vertices that are adjacent to a given vertex. The graph class should be in DGraph.java.
+		
+	}
+
+}
---
+++ TravellingSalesman/PA11Main.java
@@ -0,0 +1,11 @@
+
+public class PA11Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello World");
+	}
+	
+	
+}
