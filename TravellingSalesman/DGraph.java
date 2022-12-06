c6357bd08baaf1cf8bb26f147518f79a1beaf3ab
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-12-06 16:39:39 -0700

    added heruistinc

--- TravellingSalesman/DGraph.java
+++ TravellingSalesman/DGraph.java
@@ -1,5 +1,111 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DGraph {
+	
+	private ArrayList<LinkedList<Edge>> adjList = new ArrayList<>(); 
+	private int numVertices;
+	
+	
+	
+	DGraph(int numVertices){
+		
+//		A weighted directed graph is needed to represent the ‘cities’ and their distances. The graph
+//		should support operations that will be needed by the algorithms, mainly a method to find all
+//		vertices that are adjacent to a given vertex. The graph class should be in DGraph.java.
+		
+		this.numVertices = numVertices;
+		for(int i=0; i<numVertices; i++) {
+			adjList.add(new LinkedList<Edge>());
+		}
+		
+	}
+	
+	private class Edge{
+		int label;
+		double weight;
+		Edge(int v, double w){
+			this.label = v;
+			this.weight = w;
+		}
+	}
+	
+	
+	void addEdge(int u, int v, double w) {
+		adjList.get(u-1).add(new Edge(v,w));
+	}
+	
+	public String toString() {
+		String ret = "";
+		for(LinkedList<Edge> each : adjList) {
+			ret += "[";
+			for (Edge eachEdge : each) {
+				ret +=  "to" + eachEdge.label + " with " + eachEdge.weight + ", ";
+				
+			}
+			ret += "]";
+			
+		}
+		
+		return ret;
+		
+	}
+	
+	
+	
+	public double Heruistic() {
+		
+		List<Integer> visited = new ArrayList<>();
+		int curPosition =  1;
+		visited.add(curPosition);
+		
+		double pathCost = 0.0;
+		int bestCityToGo = 0;
+		double lastCitytoFirst = 0.0;
+		
+		
+		
+		while(visited.size() < numVertices) {
+				double curLength = Double.MAX_VALUE;
+				
+				
+				for(Edge eachEdge : adjList.get(curPosition-1)) {
+					if(eachEdge.weight<curLength && !visited.contains(eachEdge.label)) {
+						curLength  = eachEdge.weight;
+						bestCityToGo = eachEdge.label;				
+					}
+					if (eachEdge.label == 1) {
+						lastCitytoFirst = eachEdge.weight;
+						
+					}
+				}
+				
+				
+				visited.add(bestCityToGo);
+				pathCost += curLength;
+				curPosition = bestCityToGo;
+	
+		
+		}
+		
+		for(Edge eachEdge : adjList.get(bestCityToGo-1)) {
+			if(eachEdge.label==1) {
+				pathCost += eachEdge.weight;
+			}
+		}
+
+		
+		for(int each :visited) {
+			System.out.println(each);
+		}
+		System.out.println(pathCost);
+	
+		return 2.1;
+
+		
+	}
+	
 	
 	
 	
@@ -7,33 +113,36 @@ public class DGraph {
 	public void myOwn() {
 		
 		
+		
+		
 	}
 	
 	
 	
 	public void BackTracking() {
+		int cityNumber = numVertices;
+		boolean[] visited = new boolean[cityNumber];
+		
+		visited[0] = true;
+		double retVal = Double.MAX_VALUE;
+		
+				
 		
 	}
 	
+	private void recBacktrack(boolean[] visited, int curPos, double curCost)
 	
-	
-	public void Heruistic() {
-		
+	{
+			
 	}
 	
 	
+
 	
 	
 	
 	
 	
 	
-	class Graph{
-		
-//		A weighted directed graph is needed to represent the ‘cities’ and their distances. The graph
-//		should support operations that will be needed by the algorithms, mainly a method to find all
-//		vertices that are adjacent to a given vertex. The graph class should be in DGraph.java.
-		
-	}
 
 }
--- TravellingSalesman/PA11Main.java
+++ TravellingSalesman/PA11Main.java
@@ -1,11 +1,60 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PA11Main {
-
+	
+	static DGraph dg;
+	
+	public static ArrayList<String[]> myData = new ArrayList<>();
+	
 	public static void main(String[] args) {
+		try {
+			File myObj = new File("tinyRW.mtx");
+			Scanner myReader = new Scanner(myObj);
+			
+		      while (myReader.hasNextLine()) {
+		    	  
+		          String data = myReader.nextLine();
+		          
+		          if(!data.startsWith("%")) {
+		        	  myData.add(data.split("\\s+"));
+
+		          }
+		        }
+			
+		      for(String[] each : myData) {
+		    	  
+		    	  if(dg == null) {
+		    		  dg = new DGraph(Integer.parseInt(each[0]));
+
+		    	  }else {
+
+
+		    		  int u = Integer.parseInt(each[0]);
+		    		  int v =  Integer.parseInt(each[1]);
+		    		  double w = Double.parseDouble(each[2]);
+		    		  dg.addEdge(u,v,w);
+		    		  
+		    	  }
+		      }
+		      dg.Heruistic();
+		      System.out.println(dg);
+			
+		}catch(Exception e) {
+				
+			 System.out.println(e.getMessage());
+			
+		}
 		
 		
-		System.out.println("Hello World");
+		
+	
 	}
+	
+	
+	
+	
 	
 	
 }
